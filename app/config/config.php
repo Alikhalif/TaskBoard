eae7c33b33387e3167b588018cd135921f201c25
Alikhalif <73487086+Alikhalif@users.noreply.github.com>
2023-02-01 15:50:16 +0100

    Delete app directory

--- app/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('BURL',"http://taskboard.com/");
-
-define('ROOT',"http://taskboard.com/TaskBoard/public");
-
--- app/controllers/apiController.php
+++
@@ -1,9 +0,0 @@
-<?php
-include(MODELS.'/API.php');
-class ApiController{
-    public function data(){
-        $db = new Apidata();
-        $res['data'] = $db->set();
-        View::load('myApi',$res);
-    }
-}
